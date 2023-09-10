f375018e582bb140ab283bc59c7ae4e090a27f92
Biswajit Paul <biswajitpaul05@gmail.com>
2023-09-10 23:46:04 +0530

    update domestic fixed charge

--- src/config/constants.jsx
+++ src/config/constants.jsx
@@ -1,2 +1,6 @@
 export const DOMESTIC_TYPE = "domestic";
 export const COMMERCIAL_TYPE = "commercial";
+export const DOMESTIC_METER_RENT = 10;
+export const COMMERCIAL_METER_RENT = 15;
+export const DOMESTIC_FIXED_CHARGE = 37.5;
+export const COMMERCIAL_FIXED_CHARGE = 24;
